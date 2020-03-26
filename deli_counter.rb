332e30816b2f5dcf3fcd66e9cc88187059eca642
Javante Nelson <javantenelson@gmail.com>
2020-03-26 19:48:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -15,7 +15,7 @@ end
 
 def now_serving(katz_deli)
   if katz_deli > 0
-    puts "Currently serving #{katz_deli.first}."puts "There is nobody waiting to be served!"
+    puts "Currently serving #{katz_deli.first}."
   else
     
   end
